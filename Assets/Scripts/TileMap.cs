b81583086c5d1b98eb36b980b9a99b5cdd4c5623
Nikolay <nikkomyk@gmail.com>
2016-05-20 19:34:20 +0300

    Initial Setup

---
+++ Assets/Scripts/TileMap.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TileMap : MonoBehaviour {
+
+	TileType[] tileTypes;
+
+	int[,] tiles;
+
+	public int tileSizeX;
+	public int tileSizeZ;
+
+	void Start () {
+		//Tile map Allocation
+		tiles = new int[ tileSizeX, tileSizeZ];
+
+		//Tile map Initialization
+		for (int x = 0; x < tileSizeX; x++) 
+		{
+			for (int z = 0; z < tileSizeZ; z++) 
+			{
+				tiles[x,z] = 0;
+			}
+		}
+	}
+	
+	void Update () {
+	
+	}
+}
