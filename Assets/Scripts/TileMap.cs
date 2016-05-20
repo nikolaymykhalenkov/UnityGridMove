b1676f7b25f9c6b93ea8b614ec234eb9ee897b03
Nikolay <nikkomyk@gmail.com>
2016-05-20 20:16:49 +0300

    Tilemap generated

--- Assets/Scripts/TileMap.cs
+++ Assets/Scripts/TileMap.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class TileMap : MonoBehaviour {
 
-	TileType[] tileTypes;
+	public TileType[] tileTypes;
 
 	int[,] tiles;
 
@@ -22,9 +22,37 @@ public class TileMap : MonoBehaviour {
 				tiles[x,z] = 0;
 			}
 		}
+		//L shaped mountain
+		tiles[1,1] = 2;
+		tiles[1,2] = 2;
+		tiles[1,3] = 2;
+		tiles[1,4] = 2;
+		tiles[1,5] = 2;
+		tiles[2,5] = 2;
+		tiles[3,5] = 2;
+
+		tiles[8,8] = 1;
+		tiles[8,7] = 1;
+		tiles[7,8] = 1;
+		tiles[7,7] = 1;
+		tiles[7,5] = 1;
+		tiles[7,6] = 1;
+		tiles[8,6] = 1;
+
+
+		//Prefabs instantiation
+		GenerateMapVisual();
+
 	}
 	
-	void Update () {
-	
+	void GenerateMapVisual () {
+		for (int x = 0; x < tileSizeX; x++) 
+		{
+			for (int z = 0; z < tileSizeZ; z++) 
+			{
+				GameObject newTile = Instantiate(tileTypes[tiles[x,z]].tileVisualPrefab, new Vector3(x, 0, z), Quaternion.identity) as GameObject;
+				newTile.transform.parent = transform;
+			}
+		}
 	}
 }
--- Assets/Scripts/TileType.cs
+++ Assets/Scripts/TileType.cs
@@ -1,6 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[System.Serializable]
 public class TileType {
 	public string name;
+	public GameObject tileVisualPrefab;
 }
